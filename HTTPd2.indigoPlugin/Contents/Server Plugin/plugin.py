0ce5f0e6372ad93b9f85e94435c9ff5c52071b1d
Joe Keenan <joe@flyingdiver.com>
2022-12-30 13:30:01 -0500

    Fixed digest authentication

--- HTTPd2.indigoPlugin/Contents/Server Plugin/plugin.py
+++ HTTPd2.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -95,7 +95,7 @@ class MyRequestHandler(BaseHTTPRequestHandler):
             self.logger.debug(f"MyRequestHandler: Invalid authentication scheme: {auth_scheme}")
             return False
                 
-        self.logger.debug(f"MyRequestHandler: auth_map = {auth_map}")
+        self.logger.threaddebug(f"MyRequestHandler: auth_map = {auth_map}")
             
         # check username
         if auth_map["username"] != self.server.httpUser:
@@ -115,10 +115,9 @@ class MyRequestHandler(BaseHTTPRequestHandler):
                 return False
  
         elif auth_scheme == "digest":
-
-            h1 = hashlib.md5(self.server.httpUser + ":" + REALM + ":" + self.server.httpPassword).hexdigest()
-            h2 = hashlib.md5(self.command + ":" + auth_map["uri"]).hexdigest()
-            rs = h1 + ":" + auth_map["nonce"] + ":" + auth_map["nc"] + ":" + auth_map["cnonce"] + ":" + auth_map["qop"] + ":" + h2
+            h1 = hashlib.md5((self.server.httpUser + ":" + REALM + ":" + self.server.httpPassword).encode()).hexdigest()
+            h2 = hashlib.md5((self.command + ":" + auth_map["uri"]).encode()).hexdigest()
+            rs = (h1 + ":" + auth_map["nonce"] + ":" + auth_map["nc"] + ":" + auth_map["cnonce"] + ":" + auth_map["qop"] + ":" + h2).encode()
             if hashlib.md5(rs).hexdigest() == auth_map["response"]:
                 self.logger.debug(f"MyRequestHandler: {auth_scheme} Authorization valid".capitalize())
                 return True
